0f3b7cad1cb14b41ed4318046ae0b9acd4bbf32d
Sweps62 <112030088+Sweps62@users.noreply.github.com>
2025-04-02 18:28:26 +0200

    bootstrap

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ include 'templates/header.html';
                     <div class="row gx-5 align-items-center justify-content-center">
                         <div class="col-lg-8 col-xl-7 col-xxl-6">
                             <div class="my-5 text-center text-xl-start">
-                                <h1 class="display-5 fw-bolder text-white mb-2">A Bootstrap 5 template for modern businesses</h1>
-                                <p class="lead fw-normal text-white-50 mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit!</p>
+                                <h1 class="display-5 fw-bolder text-white mb-2">SAÉ DevOps</h1>
+                                <p class="lead fw-normal text-white-50 mb-4">Passez en revue les 23 rooms qui ont été faits par le groupe composé de Hugo, Quentin, Théo, et Gaëtan</p>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Get Started</a>
-                                    <a class="btn btn-outline-light btn-lg px-4" href="#!">Learn More</a>
+                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Comment on s'est organisé</a>
+                                    <a class="btn btn-outline-light btn-lg px-4" href="#!">Présentation du groupe</a>
                                 </div>
                             </div>
                         </div>
